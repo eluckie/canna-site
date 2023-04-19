0658ff33eed14eadb29edc06fb7231dfec300b2a
E Luckie <eluckie.d@gmail.com>
2023-04-19 12:43:36 -0600

    set up initial project structure

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CannaPhoto from "./../img/canna.png";
+
+function App() {
+  return (
+    <React.Fragment>
+      <img src={CannaPhoto} alt="cannabis icon" />
+    </React.Fragment>
+  );
+}
+
+export default App;
